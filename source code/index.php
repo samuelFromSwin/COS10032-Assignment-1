39ef1abff26853a55fce09fcf3ff7748e577ca26
L4wrents <105692936@student.swin.edu.au>
2026-01-28 23:23:49 +1100

    Update index.php

--- source code/index.php
+++ source code/index.php
@@ -21,7 +21,7 @@
     <main class="index">
         <h2 class="index">Modern accounting company that provides simple, reliable, and efficient financial solutions for businesses.</h2>
         <p class="index">Works best for you.</p>
-        <li class="index"><a href="https://youtu.be/a7gTM42Uei8">Youtube Video Overview</a></li>
+        <li class="index"><a href="https://www.youtube.com/watch?v=Lk9j4ZXOjBk">Youtube Video Overview</a></li>
     </main>
 
 
